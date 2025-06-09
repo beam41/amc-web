cb0ca1eb7c008e2f6cc5ac26f0780013fbb268ec
Meehoi <beam_baimon@hotmail.com>
2025-06-09 22:37:02 +0700

    add track editor page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<a href="/track-editor" class="text-blue-500 underline">Track editor</a>
