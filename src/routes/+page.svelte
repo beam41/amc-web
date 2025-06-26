30233442644d8734231d53dd42d71a49197e6797
Meehoi <beam_baimon@hotmail.com>
2025-06-26 23:02:44 +0700

    change padding to margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex w-full flex-col items-center">
   <div class="-mt-14 flex h-dvh flex-col items-center justify-center pt-14 lg:-mt-16 lg:pt-16">
-    <img src={Poster} alt="ASEAN Poster" class="w-full max-w-250 px-20 pb-20" />
+    <img src={Poster} alt="ASEAN Poster" class="w-full max-w-250 mx-20 mb-20" />
     <h1 class="text-center text-6xl font-bold">{m['site_name']()}</h1>
   </div>
 </div>
