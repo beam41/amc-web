73a191a82b8c512ae147f211ff9c93d656f03add
Meehoi <beam_baimon@hotmail.com>
2025-06-26 23:02:44 +0700

    feat: home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<a href="/track" class="text-blue-500 underline">Track editor</a>
+<script lang="ts">
+  import Poster from '$lib/assets/asean_poster.webp';
+  import { m } from '$lib/paraglide/messages';
+</script>
+
+<div class="flex w-full flex-col items-center">
+  <div class="-mt-14 flex h-dvh flex-col items-center justify-center pt-14 lg:-mt-16 lg:pt-16">
+    <img src={Poster} alt="ASEAN Poster" class="w-full max-w-250 px-20 pb-20" />
+    <h1 class="text-center text-6xl font-bold">{m['site_name']()}</h1>
+  </div>
+</div>
