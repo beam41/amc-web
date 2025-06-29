e52687f91fa38682268511790677b9ada2b386ca
Meehoi <beam_baimon@hotmail.com>
2025-06-29 18:12:11 +0700

    bounding box

--- src/lib/utils/getLocationAtPoint.ts
+++ src/lib/utils/getLocationAtPoint.ts
@@ -1,28 +1,46 @@
 import type { Vector2 } from '$lib/components/TrackEditor/types';
 import areaVolume from '$lib/assets/data/out_area_volume.json';
 
-const pointInPolygon = (point: Vector2, polygon: Vector2[]): boolean => {
-  let count = 0;
-  for (let i = 0; i < polygon.length; i += 2) {
-    const a = polygon[i];
-    const b = polygon[i + 1];
-    const y1 = a.y;
-    const y2 = b.y;
-    const x1 = a.x;
-    const x2 = b.x;
-    if (y1 > point.y !== y2 > point.y) {
-      const intersect = x1 + ((point.y - y1) * (x2 - x1)) / (y2 - y1);
-      if (intersect > point.x) {
-        count += 1;
-      }
-    }
+// Precompute bounding boxes for all areas
+const areaVolumeWithBBox = areaVolume.map((area) => {
+  let minX = Infinity,
+    minY = Infinity,
+    maxX = -Infinity,
+    maxY = -Infinity;
+  for (const v of area.vertex) {
+    if (v.x < minX) minX = v.x;
+    if (v.x > maxX) maxX = v.x;
+    if (v.y < minY) minY = v.y;
+    if (v.y > maxY) maxY = v.y;
   }
-  return count % 2 === 1;
-};
+  return { ...area, box: { minX, minY, maxX, maxY } };
+});
 
 export const getLocationAtPoint = (point: Vector2) => {
-  const matchLocations = areaVolume.filter((area) => {
-    return pointInPolygon(point, area.vertex);
+  const matchLocations = areaVolumeWithBBox.filter((area) => {
+    const inBoundingBox =
+      point.x >= area.box.minX &&
+      point.x <= area.box.maxX &&
+      point.y >= area.box.minY &&
+      point.y <= area.box.maxY;
+    if (!inBoundingBox) return false;
+
+    let count = 0;
+    for (let i = 0; i < area.vertex.length; i += 2) {
+      const a = area.vertex[i];
+      const b = area.vertex[i + 1];
+      const y1 = a.y;
+      const y2 = b.y;
+      const x1 = a.x;
+      const x2 = b.x;
+      if (y1 > point.y !== y2 > point.y) {
+        const intersect = x1 + ((point.y - y1) * (x2 - x1)) / (y2 - y1);
+        if (intersect > point.x) {
+          count += 1;
+        }
+      }
+    }
+    return count % 2 === 1;
   });
 
   let racetrack: string | undefined;
